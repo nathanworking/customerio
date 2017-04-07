42cf92d41a1953eaaf2e9fdaa1c4ce7f671874e4
nathan anderson <anderson@nathanworking.com>
2017-04-07 14:44:58 -0400

    Initial Upload
    Basics of my site - extras stripped away

---
+++ js/main.js
@@ -0,0 +1,29 @@
+jQuery(document).ready(function($){
+	var isLateralNavAnimating = false;
+
+	//open/close lateral navigation
+	$('.cd-nav-trigger').on('click', function(event){
+		event.preventDefault();
+		//stop if nav animation is running
+		if( !isLateralNavAnimating ) {
+			if($(this).parents('.csstransitions').length > 0 ) isLateralNavAnimating = true;
+
+			$('body').toggleClass('navigation-is-open');
+			$('.cd-navigation-wrapper').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+				//animation is over
+				isLateralNavAnimating = false;
+			});
+		}
+	});
+});
+
+// Slider (swiper)
+var swiper = new Swiper('.swiper-container', {
+    pagination: '.swiper-pagination',
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev',
+    slidesPerView: 1,
+    paginationClickable: true,
+    spaceBetween: 30,
+    loop: true
+});
